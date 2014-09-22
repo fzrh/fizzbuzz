f551bbf02e120129f05d14e076de6b1c7e02eaa4
Faezrah Rizalman <fwrizalman@gmail.com>
2014-09-22 21:55:54 +0800

    Updated rspec be_truthy

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -5,15 +5,15 @@ describe 'FizzBuzz' do
 	context 'knows if a number is divisible by' do
 
 		it '3' do
-			expect(is_divisible_by_three?(3)).to be_true
+			expect(is_divisible_by_three?(3)).to be_truthy
 		end
 
 		it '5' do
-			expect(is_divisible_by_five?(5)).to be_true
+			expect(is_divisible_by_five?(5)).to be_truthy
 		end
 
 		it '15' do
-			expect(is_divisible_by_fifteen?(15)).to be_true
+			expect(is_divisible_by_fifteen?(15)).to be_truthy
 		end
 
 	end
@@ -21,15 +21,15 @@ describe 'FizzBuzz' do
 	context 'knows 1 is not divisible by' do
 
 		it '3' do
-			expect(is_divisible_by_three?(1)).not_to be_true
+			expect(is_divisible_by_three?(1)).not_to be_truthy
 		end
 
 		it '5' do
-			expect(is_divisible_by_five?(1)).not_to be_true
+			expect(is_divisible_by_five?(1)).not_to be_truthy
 		end
 
 		it '15' do
-			expect(is_divisible_by_fifteen?(1)).not_to be_true
+			expect(is_divisible_by_fifteen?(1)).not_to be_truthy
 		end
 
 	end
